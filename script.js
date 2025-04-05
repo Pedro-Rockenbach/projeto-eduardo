f6cfe180ebff06ee794118610914691ed9999f89
Pedro-Rockenbach <pedrorbach@gmail.com>
2025-04-05 09:34:25 -0300

    rick roll with no ads

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ document.getElementById('quiz-form').addEventListener('submit', function(event) 
     if (respostaSelecionada === respostaCorreta) {
         window.location.href = 'interlude.html'; // Redireciona para a página de acerto
     } else {
-        window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'; // Redireciona para a página de erro
+        window.location.href = 'https://www.youtube.com/watch?v=xvFZjo5PgG0'; // Redireciona para a página de erro
     }
 });
